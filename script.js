2638440e479d5decc84c8da62adaab14dcbfb246
Subhajit Beshra <subhajitbeshra21@gmail.com>
2026-03-30 22:08:43 +0530

    Added some changes

--- script.js
+++ script.js
@@ -7,52 +7,30 @@ if (slider && countValue) {
         countValue.textContent = slider.value;
     });
 }
-
-function toggleDarkMode() {
-      document.body.classList.toggle("dark");
-    }
-
-    function generateUsername() {
-      const mode = document.getElementById("mode").value;
-      const input = document.getElementById("inputText").value.trim();
-      const resultsDiv = document.getElementById("results");
-      resultsDiv.innerHTML = ""; // Clear previous
-
-      let suggestions = [];
-
-      if (mode === "normal" && input) {
-        suggestions = [
-          input + "_123",
-          input + "Pro",
-          "The_" + input,
-          input + "_X",
-          input + "Official"
-        ];
-      } else if (mode === "hard" && input) {
-        suggestions = [
-          input + "Master",
-          input + "King",
-          input + "_Zone",
-          "iAm" + input,
-          input + "_HQ"
-        ];
-      } else if (mode === "harder") {
-        const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        for (let i = 0; i < 5; i++) {
-          let randomName = "";
-          for (let j = 0; j < 10; j++) {
-            randomName += chars.charAt(Math.floor(Math.random() * chars.length));
-          }
-          suggestions.push(randomName);
-        }
-      } else {
-        suggestions = ["⚠️ Please enter a name or keyword"];
-      }
-
-      suggestions.forEach(name => {
-        const div = document.createElement("div");
-        div.className = "username";
-        div.textContent = name;
-        resultsDiv.appendChild(div);
-      });
-    }
+document.querySelectorAll(".dropdown").forEach(t =>{
+  t.addEventListener('click', () =>{
+    document.querySelectorAll(".dropdown").forEach(x =>{
+      x.classList.remove('active');
+    })
+    t.classList.add('active');
+  })
+})
+document.querySelectorAll(".material-symbols-outlined").forEach(t => {
+  t.addEventListener('click', () => {
+    document.querySelectorAll(".material-symbols-outlined").forEach(x => {
+      x.classList.remove('active');
+    })
+    t.classList.add('active');
+  })
+})
+function generateUsernames(){
+  document.getElementsByClassName("title-2")[0].style.display = "none";
+  document.getElementsByClassName("loader")[0].style.display = "block";
+  setTimeout(() =>{
+      document.getElementsByClassName("loader")[0].style.display = "none";
+      document.getElementsByClassName("output-results")[0].style.display = "block";
+  }, 1000);
+}
+function copyUsername(id){
+  alert("Username Copied: ");
+}
